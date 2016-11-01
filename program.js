4527679741dccd4085568d9cf88058408c841823
李学江 <lixuejiang@ggbond-mac.local>
2016-11-01 14:33:25 +0800

    FILTERED LS

--- program.js
+++ program.js
@@ -1,8 +1,16 @@
 var fs = require('fs'),
-	filename = process.argv[2];
-	fs.readFile(filename, function(err, data){
-		if (err) {
-			throw err;
-		};
-		console.log(data.toString().split('\n').length -1);
+	path = require('path'),
+	dir = process.argv[2],
+	ext = process.argv[3];
+
+fs.readdir(dir, function(err, list){
+	if (err) {
+		throw err;
+	};
+
+	list.filter(function(item, index){
+		if(path.extname(item) === '.' + ext){
+			console.log(item);
+		}
 	});
+});
