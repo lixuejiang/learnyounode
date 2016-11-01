274bb08f92c97c2973052865535136265f678a2b
李学江 <lixuejiang@ggbond-mac.local>
2016-11-01 19:34:28 +0800

    time server

--- program.js
+++ program.js
@@ -1,19 +1,11 @@
-var http = require('http'),
-	async = require('async'),
-	bl = require('bl'),
-	urls = process.argv.slice(2);
+var net = require('net'),
+	strftime = require('strftime'),
+	port = process.argv[2];
 
-async.map(urls,function(url,callback){
-	http.get(url,function(response){
-		response.pipe(bl(function (err, data) {
-			if(err){
-				callback(err);
-			}
-			callback(null,data.toString());
-		}))  
-	});
-},function(err,results){
-	results.map(function(item,index){
-		console.log(item);
-	});
-})
+var server = net.createServer(function(socket) {
+	var str = strftime('%F %H:%M');
+	str = str + '\n';
+	socket.write(str);
+	socket.end();
+})  
+server.listen(port);
