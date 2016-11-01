22f70107f36fdb3ec608d83718c94b7aa43733c5
李学江 <lixuejiang@ggbond-mac.local>
2016-11-01 14:21:09 +0800

    MY FIRST ASYNC I/O

--- program.js
+++ program.js
@@ -1,9 +1,8 @@
 var fs = require('fs'),
 	filename = process.argv[2];
-var buffer = fs.readFileSync(filename).toString().split('\n');
-
-if(buffer.length > 0){
-	console.log(buffer.length -1);
-}else{
-	console.log(buffer.length);
-}
+	fs.readFile(filename, function(err, data){
+		if (err) {
+			throw err;
+		};
+		console.log(data.toString().split('\n').length -1);
+	});
